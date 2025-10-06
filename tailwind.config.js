4bf7236c07c0ae7370ca8dbf263eb629708818f8
Brandon3g <bgonzalez022@gmail.com>
2025-10-05 23:28:24 -0700

    Overrode DaisyUI’s light and dark theme tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
+import themes from "daisyui/src/theming/themes.js";
+
+const { light, dark } = themes;
 
 export default {
   content: [
@@ -18,6 +21,23 @@ export default {
   theme: { extend: {} },
   plugins: [daisyui],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      {
+        light: {
+          ...light,
+          primary: "#7C3AED",
+          "primary-focus": "#6D28D9",
+          "primary-content": "#ffffff",
+        },
+      },
+      {
+        dark: {
+          ...dark,
+          primary: "#7C3AED",
+          "primary-focus": "#6D28D9",
+          "primary-content": "#ffffff",
+        },
+      },
+    ],
   },
 };
